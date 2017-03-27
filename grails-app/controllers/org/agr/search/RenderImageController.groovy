eb4b432fd655e4a34f16460b18061eeda937ba44
Nathan Dunn <ndunn@me.com>
2017-03-26 22:18:59 -0700

    initial checking

--- grails-app/controllers/org/agr/search/RenderImageController.groovy
+++ grails-app/controllers/org/agr/search/RenderImageController.groovy
@@ -2,11 +2,50 @@ package org.agr.search
 
 class RenderImageController {
 
+    String imageDirectory = "/var/jbrowse/images"
+    // support: http://localhost/jbrowse/overview.html?data=data%2FDanio%20rerio&loc=12:47426569..47433060&tracks=All%20Genes&highlight=&lookupSymbol=chrm3b
     def index() {
+        String requestUrl = request.url
+
+        // 1. check cache and return
+        String species = getSpecies(requestUrl)
+        String location = getLocation(requestUrl)
+        File imageFile = new File(imageDirectory+"/"+species+"/"+location)
+        if(imageFile.exists()){
+            render file: imageFile.bytes, contentType: 'image/png'
+            return
+        }
+
+
+        // 2. generate image to the directory
+//        phantomjs rasterize.js "http://localhost/jbrowse/overview.html?data=data%2FDanio%20rerio&loc=12:47426569..47433060&tracks=All%20Genes&highlight=&lookupSymbol=chrm3b" jbrowse.png '4800px*2600px' 4 5000
+    // convert jbrowse.png -trim +repage jbrowse_trim.png && open jbrowse_trim.png
+        File tempFile = File.createTempFile()
+        def command = """phantomjs rasterize.js ${requestUrl} ${tempFile.absolutePath} '4800px*2600px' 4 5000"""// Create the String
+        def proc = command.execute()                 // Call *execute* on the string
+        proc.waitFor()
+
+        def command2 = """convert ${tempFile} +repage ${imageFile}"""// Create the String
+        def proc2 = command2.execute()                 // Call *execute* on the string
+        proc2.waitFor()
+
+        if(imageFile.exists()){
+            render file: imageFile.inputStream, contentType: 'image/png'
+        }
+        else{
+            throw new RuntimeException("Failed to create image")
+        }
 
     }
 
-    def renderImage(){
-
+    String getLocation(String s) {
+        return "12:47426569..47433060"
     }
+
+    String getSpecies(String s) {
+        return "Danio%20rerio"
+    }
+//    def renderImage(){
+//
+//    }
 }
